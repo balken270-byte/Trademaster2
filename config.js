ecc418aaf3e532e3e698c0d95737899f6bc1edca
balken270-byte <balken270@gmail.com>
2026-01-10 21:21:14 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+// TRADEVIA YAPILANDIRMA DOSYASI
+// Bu dosya API anahtarlarını ve genel ayarları tutar.
+
+const AppConfig = {
+    // CoinGecko API Anahtarı
+    // Güvenlik İçin: CoinGecko panelinden bu anahtarı sadece kendi domainine kısıtla!
+    COINGECKO_KEY: "CG-G2hRKmRzcPTYJUA3L79dg8Ta",
+
+    // Google Drive Yedekleme Ayarları
+    GOOGLE_CLIENT_ID: '188241284734-3tdriq9svbm1vh8acq2vqm80lieo5v7u.apps.googleusercontent.com',
+    GOOGLE_API_KEY: 'AIzaSyCCxeYq1C4mnv-3CRvTDertau_Jlv11OaA',
+
+    // Opsiyonel: Varsayılan Ayarlar
+    DEFAULT_CURRENCY: 'TRY',
+    DEFAULT_LANG: 'tr'
+};
